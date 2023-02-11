e76ef5e7bba81929a86755cd64a991e2e3938dd4
chintapatlasumanrao <91577391+chintapatlasumanrao@users.noreply.github.com>
2023-02-10 22:31:39 -0700

    MONG-2 update operation

--- src/main/java/com/springmongo/repository/impl/EmployeeUpdateRepositoryImpl.java
+++ src/main/java/com/springmongo/repository/impl/EmployeeUpdateRepositoryImpl.java
@@ -4,9 +4,11 @@ import com.springmongo.model.Address;
 import com.springmongo.model.Employee;
 import com.springmongo.repository.EmployeeUpdateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.FindAndModifyOptions;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -21,16 +23,17 @@ public class EmployeeUpdateRepositoryImpl implements EmployeeUpdateRepository {
             Query query = new Query();
             query.addCriteria(Criteria.where("employeeId").is(employeeId));
 
-            Employee emp = mongoTemplate.findOne(query, Employee.class);
-            emp.setAddress(address);
-            mongoTemplate.save(emp);
+            Update update = new Update();
+            update.set("address", address);
 
+            Employee emp = mongoTemplate.findAndModify(query, update, new FindAndModifyOptions().returnNew(true), Employee.class);
             return emp;
-
         } catch(Exception exception) {
             // catch exception
         }
         return null;
     }
+
+
 
 }
